60861073138b4c859cfc031bf2fd25fbf716df39
eschbach-d <dennis.eschbach@tu-dortmund.de>
2022-03-24 21:05:46 +0100

    schnitt teil1

--- Aufgabe1/Simulation/Linie.py
+++ Aufgabe1/Simulation/Linie.py
@@ -4,10 +4,11 @@ class Linie():
 
     # Ortsvektor und Richtungsvektor = nparray[3], parameter = nparray[0,1]
     # Achtung: Richtungsvektor muss normiert werden
-    def __init__(self,ortsvektor,richtungsvektor,parameter):
+    def __init__(self,ortsvektor,richtungsvektor,parameter, richtung):
         self.ortsvektor = ortsvektor
         self.richtungsvektor = richtungsvektor
         self.parameter = parameter
+        self.richtung = richtung
 
     #Getter und Setter
 
@@ -20,6 +21,9 @@ class Linie():
     def getParameter(self):
         return self.parameter
 
+    def getRichtung(self):
+        return self.richtung
+
     def setOrtsvektor(self, value):
         self.ortsvektor = value
 
@@ -29,12 +33,31 @@ class Linie():
     def setParameter(self,value):
         self.Parameter = value
 
+    def setRichtung(self,value):
+        self.richtung = value
+
     #Methoden
 
-    def schneide(self, value):
+    def schneide(self, gerade):
         # Geraden sind parallel:
-        if np.array_equal(self.richtungsvektor, value.richtungsvektor):
-            return 1
+        if self.richtung == gerade.richtung:
+            zeile1 = np.empty(3, dtype= float)
+            zeile2 = np.empty(3, dtype= float)
+            zeile3 = np.empty(3, dtype= float)
+
+            zeile1[1] = 0 - self.richtungsvektor[0]
+            zeile2[1] = 0 - self.richtungsvektor[1]
+            zeile3[1] = 0 - self.richtungsvektor[2]
+
+            zeile1[0] = gerade.richtungsvektor[0]
+            zeile2[0] = gerade.richtungsvektor[1]
+            zeile3[0] = gerade.richtungsvektor[2]
+
+            zeile1[2] = self.ortsvektor[0] - gerade.ortsvektor[0]
+            zeile2[2] = self.ortsvektor[1] - gerade.ortsvektor[1]
+            zeile3[2] = self.ortsvektor[2] - gerade.ortsvektor[2]
+
+            if()
         # Geraden schneiden sich im rechten Winkel:
 
 
@@ -44,12 +67,12 @@ class Linie():
         self.ortsvektor[2] = self.ortsvektor[2] + value[2]
 
     def getKoordinaten(self):
-        ausgabe = np.empty(len(self.parameter)*3,dtype = int)
+        ausgabe = np.empty(len(self.parameter)*3,dtype = float)
 
         for i in range(0,len(ausgabe),3):
-            ausgabe[i] = self.ortsvektor[0] + self.richtungsvektor[0]
-            ausgabe[i+1] = self.ortsvektor[1] + self.richtungsvektor[1]
-            ausgabe[i+2] = self.ortsvektor[2] + self.richtungsvektor[2]
+            ausgabe[i] = self.ortsvektor[0] + self.parameter[i/3] * self.richtungsvektor[0]
+            ausgabe[i+1] = self.ortsvektor[1] + self.parameter[i/3] * self.richtungsvektor[1]
+            ausgabe[i+2] = self.ortsvektor[2] + self.parameter[i/3] *  self.richtungsvektor[2]
 
         return ausgabe
 
