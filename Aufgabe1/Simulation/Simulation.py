522d72fa3a4578461e91cc4949b0ce8004f1ea34
eschbach-d <dennis.eschbach@tu-dortmund.de>
2022-03-24 16:42:26 +0100

    Multidexelboard
    Cut muss noch ausgearbeitet werden
    vielleicht Offset in variable speichern
    und eine upadte methode hinzuufügen

--- Aufgabe1/Simulation/Simulation.py
+++ Aufgabe1/Simulation/Simulation.py
@@ -6,3 +6,15 @@
 
 # Hier soll die Funktion Step und matplotlib benutzt werden um eine
 # 3D Animation zu erstellen
+
+import numpy as np
+import Werkstück
+import Werkzeug
+import Animation
+
+class Simulation():
+
+    def __init__(self, ablauf):
+        self.zylinder = Werkzeug(30,8,10,0.1991,0.3660,100,0,0,0,0)
+        self.quader = Werkstück(70,30,30,10,0,0,0,0,0,0,0)
+        self.animation = Animation(self.quader, self.zylinder, ablauf, 0 )
