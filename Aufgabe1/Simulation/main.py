65009d1c941f219e0d7759ecb5fbf881d3284551
ahmed <ahmed.abuabdu@gmail.com>
2022-03-26 11:52:03 +0100

    main

--- Aufgabe1/Simulation/main.py
+++ Aufgabe1/Simulation/main.py
@@ -5,12 +5,18 @@ import readCSV
 import numpy as np
 
 
-def start(laengews, breite, hoehe, aufloesung, laengewz, durchmesser):
-
+def start(laengews, breite, hoehe, laengewz, durchmesser, aufloesung):
     ws = Werkstück.Werkstück(hoehe, laengews, breite, aufloesung)
     wz = Werkzeug.Werkzeug(laengewz, durchmesser, aufloesung)
 
     ablauf = readCSV.getPosFromCsv()
+
+    for i in range(1, len(ablauf), 1):
+        # wenn der abstand zum vorherigen Punkt des Werkzeugs zu groß ist, muss ein Zwischenschritt eingefügt werden
+        if
+
     animation = Animation.Animation(ws, wz, ablauf, 0)
 
 
+if __name__ == '__main__':
+    start(70, 70, 30, 30, 8, 10)
