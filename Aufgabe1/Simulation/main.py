9b41baf21cbd56e78e495bf49b34f11217530eb7
ahmed <ahmed.abuabdu@gmail.com>
2022-03-26 10:55:52 +0100

    main

--- Aufgabe1/Simulation/main.py
+++ Aufgabe1/Simulation/main.py
@@ -5,11 +5,12 @@ import readCSV
 import numpy as np
 
 
-def start(laengeWs, breite, hoehe, aufloesung, laengeWz, durchmesser):
-    ws = Werkstück(hoehe, laengeWs, breite, aufloesung)
-    wz = Werkzeug(laengeWz, durchmesser, aufloesung)
+def start(laengews, breite, hoehe, aufloesung, laengewz, durchmesser):
+
+    ws = Werkstück.Werkstück(hoehe, laengews, breite, aufloesung)
+    wz = Werkzeug.Werkzeug(laengewz, durchmesser, aufloesung)
 
     ablauf = readCSV.getPosFromCsv()
-    animation = Animation(ws, wz, ablauf, 0)
+    animation = Animation.Animation(ws, wz, ablauf, 0)
 
 
